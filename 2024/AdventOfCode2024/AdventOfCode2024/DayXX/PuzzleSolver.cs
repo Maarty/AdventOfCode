b2577be52afd2d5fa0941553f04d4f9f5f96b222
Martin Solarik <martin.solarik@rws.com>
2024-12-09 16:11:50 +0100

    Day XX template

---
+++ 2024/AdventOfCode2024/AdventOfCode2024/DayXX/PuzzleSolver.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode2024.DayXX
+{
+    public class PuzzleSolver : PuzzleSolverBase
+    {
+        public override string Day => "";
+
+        protected override string SolvePuzzle(string input)
+        {
+            return Solve(input);
+        }
+
+        protected override string SolvePuzzleExtended(string input)
+        {
+            return Solve(input);
+        }
+
+        private string Solve(string input)
+        {
+            long result = 0;
+
+            return result.ToString();
+        }
+
+        public override async ValueTask<List<(string Expected, string Actual)>> SolveTestsAsync()
+        {
+            return
+            [
+                ("3749", SolvePuzzle(await LoadTestInputAsync(1))),
+                //("11387", SolvePuzzleExtended(await LoadTestInputAsync(1)))
+            ];
+        }
+    }
+}
