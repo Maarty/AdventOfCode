71ff0070976730dd73e865f7a506f2316977416e
Martin Solarik <martin.solarik@rws.com>
2025-12-05 16:10:36 +0100

    Day 01

--- 2025/AdventOfCode2025/Day01/PuzzleSolver.cs
+++ 2025/AdventOfCode2025/Day01/PuzzleSolver.cs
@@ -11,22 +11,70 @@ public class PuzzleSolver : PuzzleSolverBase
 
     protected override string SolvePuzzleExtended(string input)
     {
-        return Solve(input);
+        return Solve(input, true);
     }
 
-    private string Solve(string input)
+    private string Solve(string input, bool allZeros = false)
     {
-        long result = 0;
+        var currentPosition = 50;
+        var numberOfZerosHit = 0;
+        var numberOfZerosStopped = 0;
+        
+        var lines = GetLinesInput(input).ToList();
 
-        return result.ToString();
+        foreach (var line in lines)
+        {
+            var direction = line[0];
+            var distance = int.Parse(line[1..]);
+            var normalizedDistance = distance % 100;
+            numberOfZerosHit += distance / 100;
+            
+            switch (direction)
+            {
+                case 'L':
+                    currentPosition -= normalizedDistance;
+                    if (currentPosition < 0)
+                    {
+                        if (currentPosition + normalizedDistance != 0)
+                        {
+                            numberOfZerosHit++;
+                        }
+                        currentPosition = (currentPosition + 100) % 100;
+                    }
+                    if (currentPosition == 0)
+                        numberOfZerosHit++;
+                    break;
+                case 'R':
+                    currentPosition += normalizedDistance;
+                    if (currentPosition > 100)
+                    {
+                        numberOfZerosHit++;
+                        currentPosition %= 100;
+                    }
+                    if (currentPosition == 100)
+                    {
+                        currentPosition = 0;
+                        numberOfZerosHit++;
+                    }
+                    break;
+            }
+
+            if (currentPosition == 0)
+            {
+                numberOfZerosStopped++;
+            }
+        }
+        
+
+        return allZeros ? numberOfZerosHit.ToString() : numberOfZerosStopped.ToString();
     }
 
     public override async ValueTask<List<(string Expected, string Actual)>> SolveTestsAsync()
     {
         return
         [
-            ("XXXX", SolvePuzzle(await LoadTestInputAsync(1))),
-            //("XXXX", SolvePuzzleExtended(await LoadTestInputAsync(1)))
+            ("3", SolvePuzzle(await LoadTestInputAsync(1))),
+            ("6", SolvePuzzleExtended(await LoadTestInputAsync(1)))
         ];
     }
 }
